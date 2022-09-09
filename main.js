e41490d5498c99c726f5cd637a0f1b7a814664c5
bsshapiro <bsshapiro7@gmail.com>
2022-09-08 21:07:38 -0500

    Added css

---
+++ main.js
@@ -0,0 +1,11 @@
+const aboutBtn = document.getElementsByClassName('about-me-btn');
+
+aboutBtn[0].addEventListener('click', () => {
+
+})
+
+const projectsBtn = document.getElementsByClassName('projects-btn');
+
+projectsBtn[0].addEventListener('click', () => {
+
+})
