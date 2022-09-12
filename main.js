a7e52153fdaeaf607b81d3e0ca0502ef647946d1
bsshapiro <bsshapiro7@gmail.com>
2022-09-12 17:40:46 -0500

    Started building out the projects page.

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
-const aboutBtn = document.getElementsByClassName('about-me-btn');
+const aboutBtnFunc = () => {
+  const aboutBtn = document.getElementsByClassName("about-me-btn");
 
-aboutBtn[0].addEventListener('click', () => {
+  aboutBtn[0].addEventListener("click", () => {});
+};
 
-})
+aboutBtnFunc();
 
-const projectsBtn = document.getElementsByClassName('projects-btn');
 
-projectsBtn[0].addEventListener('click', () => {
+const projectsBtnFunc = () => {
+  const projectsBtn = document.getElementsByClassName("projects-btn");
 
-})
+  projectsBtn[0].addEventListener("click", () => {});
+};
+
+projectsBtnFunc();
+
+
+const imageCarousel = () => {
+  const slidesContainer = document.getElementById("slides-container");
+  const slide = document.querySelector(".slide");
+  const prevButton = document.getElementById("slide-arrow-prev");
+  const nextButton = document.getElementById("slide-arrow-next");
+
+  nextButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft += slideWidth;
+  });
+
+  prevButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft -= slideWidth;
+  });
+};
+
+imageCarousel();
+
